39ecd6c7fd14e1445a1c7fc80d253dab818a297e
bsaxen <benny.saxen@gmail.com>
2018-12-25 13:36:11 +0100

    Update web_page.php

--- web_page.php
+++ web_page.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 //=============================================
-// File.......: infomodel.php
-// Date.......: 2018-12-23
+// File.......: web_template.php
+// Date.......: 2018-12-25
 // Author.....: Benny Saxen
 // Description:
 //=============================================
@@ -17,12 +17,12 @@ $ts           = date_format($date, 'Y-m-d H:i:s');
 //=============================================
 
 //=============================================
-function asd($domain)
+function example($x)
 //=============================================
 {
 
 }
-/=============================================
+//=============================================
 // End of library
 //=============================================
 
@@ -40,7 +40,8 @@ if (isset($_GET['do']))
 
   }
 }
-if (isset($_POST['do'])) 
+
+if (isset($_POST['do']))
 {
   $do = $_POST['do'];
 
@@ -48,7 +49,6 @@ if (isset($_POST['do']))
   {
 
   }
-
 }
 //=============================================
 // Front-End
@@ -58,7 +58,24 @@ echo "<html>
       <title>InfoModel</title>
    </head>
    <body> ";
-echo "</body></html>";
-// End of file
 
+echo("<h1>Web Template</h1>");
+echo ("<br><a href=infomodel.php?do=some&a=x>test_link</a>");
+echo "<br><br>
+   <table border=0>";
+echo "
+   <form action=\"#\" method=\"post\">
+     <input type=\"hidden\" name=\"do\" value=\"send_message\">
+     <tr><td>A</td><td> <input type=\"text\" name=\"a\" value=$a></td>
+     <tr><td>B</td><td> <input type=\"text\" name=\"b\" value=$b></td>
+     <tr><td>C</td><td> <input type=\"text\" name=\"c\" ></td>
+     <tr><td>D</td><td> <input type=\"text\" name=\"d\"></td>
+     <td><input type= \"submit\" value=\"Send\"></td></tr>
+   </form>
+   </table>";
+
+//=============================================
+// End of file
+//=============================================
+echo "</body></html>";
 ?>
